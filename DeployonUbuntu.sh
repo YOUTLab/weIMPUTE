d03b9de6dcfd2ccf5d0cd0a5b1de6dc001f3d141
YOUTLab <141199195+YOUTLab@users.noreply.github.com>
2023-08-27 09:48:17 +0800

    Add files via upload

---
+++ DeployonUbuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo docker load -i suanfa_v6.tar
+sudo docker run -d --privileged --net=host suanfa:v6
+
+##get the ID of the current Docker
+CONTAINER_ID=$(sudo docker ps -a | grep "suanfa:v6" | awk '{print $1}' | sed -n '1p')
+
+## start nginx
+sudo docker exec $CONTAINER_ID /bin/bash -c '/usr/local/nginx/sbin/nginx'
+
+## start mysql
+sudo docker exec $CONTAINER_ID /bin/bash -c '/etc/init.d/mysqld start'
+
+## start redis
+sudo docker exec $CONTAINER_ID /bin/bash -c '/usr/local/bin/redis-server /usr/local/redis/redis.conf'
+
+## start java
+sudo docker exec $CONTAINER_ID /bin/bash -c 'nohup /usr/local/jdk/bin/java -Xms256m -Xmx512m -jar /usr/local/src/suanfa/jiyin-admin.jar > /usr/local/src/suanfa/suanfa.log 2>&1'
+
+
